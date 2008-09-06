4907bcf2cae2a0f49605acfa1b6f228e3878a317
Chris Eppstein <chris@eppsteins.net>
2008-09-06 14:29:15 -0700

    I guess cells was already at 1.0 and undergoing a 1.1 release, I'm leapfrogging to 1.3 with a 1.4 imminent

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,17 @@
 module Cell
+	# minor version
+	#   * When odd, the project is under development
+	#   * When even, the project is released and will be maintained until the next even numbered release.
+	# version status:
+	#   * alpha - API for the current version is not set
+	#   * beta - API for the current version is set, bug may exists.
+	#   * rc# - Release Candidate
+  #   * stable - Mostly bug free, will be maintained by point releases.
   VERSION = {
     :major => 1,
-    :minor => 0,
+    :minor => 3,
     :micro => 0,
-    :label => 'alpha',
-    :string => "1.0.0-alpha"
+    :status => 'alpha',
+    :string => "1.3.0-alpha"
   }
 end
